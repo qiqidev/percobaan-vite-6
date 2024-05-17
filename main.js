4bc413353257d39f35d75d7e3df00b6bdfb5ee92
qiqidev <rifqiqurrotaayuni@gmail.com>
2024-05-17 12:36:25 +0700

    qiqidev

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
      
     </a>
-    <h1>Hello brow!</h1>
+    <h1 id="hello">Hello brow!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
